e4434c285c276d0fddbfbb641f32308169fe2fee
gsujanprdxn <sujang@prdxn.com>
2020-09-18 19:04:36 +0530

    hamberger menu is added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,18 @@
 
 */
 
-
+document.querySelector('.hamburger-menu').addEventListener('click',()=>{
+    var navbar = document.querySelector('nav');
+    if (navbar.style.display === "none"){
+        navbar.style.display = "flex";
+        document.querySelector('.hamburger-menu').classList.add("change")
+        
+    } else {
+        navbar.style.display = "none";
+        document.querySelector('.hamburger-menu').classList.remove("change")
+    }
+  
+  })
 
 
 
