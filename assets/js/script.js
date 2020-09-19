babf31cca634df4df51d98b8826382e9dffe733e
gsujanprdxn <sujang@prdxn.com>
2020-09-19 10:34:34 +0530

    hamberger menu js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 /* Author: 
 
 */
+document.querySelector('.hamburger-menu').addEventListener('click',()=>{
+    var navbar = document.querySelector('nav');
+    if (navbar.style.display === "none"){
+        navbar.style.display = "flex";
+        document.querySelector('.hamburger-menu').classList.add("change")
+        
+    } else {
+        navbar.style.display = "none";
+        document.querySelector('.hamburger-menu').classList.remove("change")
+    }
+  })
+
+
 
 
 
