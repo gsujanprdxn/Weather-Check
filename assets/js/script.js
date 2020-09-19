7f55e60ab1f513fad97a45fbca50792d90427506
gsujanprdxn <sujang@prdxn.com>
2020-09-19 10:20:03 +0530

    navbar style changes to verticle

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,6 @@
 
 */
 
-document.querySelector('.hamburger-menu').addEventListener('click',()=>{
-    var navbar = document.querySelector('nav');
-    if (navbar.style.display === "none"){
-        navbar.style.display = "flex";
-        document.querySelector('.hamburger-menu').classList.add("change")
-        
-    } else {
-        navbar.style.display = "none";
-        document.querySelector('.hamburger-menu').classList.remove("change")
-    }
-  
-  })
-
-
-
-
-
-
-
 
 
 
